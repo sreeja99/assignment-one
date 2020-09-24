bf198738ca70d4c8a6eee5de4306e080ddf8061e
sreeja99 <srijagodishala@gmail.com>
2020-09-24 15:25:42 +0530

    Added:Calculated Daily Employee Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -6,17 +6,21 @@ public class assignOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int dailyempwage = 0;
 		System.out.println("Welcome to Employee Wage Computation program");
-		double empCheck = Math.floor(Math.random() * 10) %3;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		if( empCheck == 1){
 			System.out.println("Employee is present");
 		}
 		else {
 			System.out.println("Employee is absent");
 		}
+		dailyempwage = FULL_DAY_HOUR * WAGE_PER_HOUR ;
+		System.out.println("full time employee wage-"+ dailyempwage);
+		
+
 
 
 	}
 
 }
-
