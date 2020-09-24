b6671c9e2e23ca6c36ed6d86898a1e47390e4f43
sreeja99 <52079776+sreeja99@users.noreply.github.com>
2020-09-24 15:33:02 +0530

    Update EmployeeWageComputation.java

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,20 +3,24 @@ package first;
 public class assignOne {
 	public static final int WAGE_PER_HOUR = 20;
 	public static final int FULL_DAY_HOUR = 8;
+	public static final int PART_DAY_HOUR = 4;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int dailyempwage = 0;
 		System.out.println("Welcome to Employee Wage Computation program");
-		double empCheck = Math.floor(Math.random() * 10) % 3;
+		int empCheck = (int) (Math.floor(Math.random() * 10) %3);
+		int dailyempwage = 0;
+		int partempwage = 0;
 		if( empCheck == 1){
 			System.out.println("Employee is present");
 		}
 		else {
 			System.out.println("Employee is absent");
 		}
-		dailyempwage = FULL_DAY_HOUR * WAGE_PER_HOUR ;
+		dailyempwage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+		partempwage = WAGE_PER_HOUR * PART_DAY_HOUR;
 		System.out.println("full time employee wage-"+ dailyempwage);
+		System.out.println("parttime employee wage-"+ partempwage);
 		
 
 
