4dca5c219779ee993d605ae25922ddeb889be786
sreeja99 <srijagodishala@gmail.com>
2020-09-24 15:36:59 +0530

    Modified:if-else is modified to switch-case

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,24 +3,45 @@ package first;
 public class assignOne {
 	public static final int WAGE_PER_HOUR = 20;
 	public static final int FULL_DAY_HOUR = 8;
+	public static final int PART_DAY_HOUR = 4;
+	public static final int IS_FULL_TIME =1;
+	public static final int IS_PART_TIME=2;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int dailyempwage = 0;
 		System.out.println("Welcome to Employee Wage Computation program");
 		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if( empCheck == 1){
-			System.out.println("Employee is present");
-		}
-		else {
-			System.out.println("Employee is absent");
-		}
-		dailyempwage = FULL_DAY_HOUR * WAGE_PER_HOUR ;
-		System.out.println("full time employee wage-"+ dailyempwage);
+		int dailyempwage = 0;
+		int noOfHour =0;
+
+//		if( empCheck == 1){
+//			System.out.println("Employee is present");
+//		}
+//		else {
+//			System.out.println("Employee is absent");
+//		}
 		
-
-
-
+//		System.out.println("full time employee wage-"+ dailyempwage);
+//		System.out.println("part time employee wage-"+ partempwage);
+	    switch((int)empCheck) {
+	        case IS_FULL_TIME :
+	    	   System.out.println("Employer is present");
+	    	   noOfHour = FULL_DAY_HOUR;
+	    	   break;
+	        case IS_PART_TIME:
+	    	   System.out.println("Employer is absent");
+	    	   noOfHour = PART_DAY_HOUR;
+	    	   break;
+	        default:
+	    	   System.out.println("Employer is absent");
+	    	
+	    }
+	    dailyempwage = WAGE_PER_HOUR * noOfHour;
+	    System.out.println("Daily wage-"+ dailyempwage);
 	}
-
 }
+
+
+
+
+
