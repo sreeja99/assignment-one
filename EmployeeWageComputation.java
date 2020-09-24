d6dd690d7bd88b318567f028a9ff7c811d097bf0
sreeja99 <srijagodishala@gmail.com>
2020-09-24 15:44:44 +0530

    Added:Calculate Monthly Employee Wage Added

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -6,14 +6,16 @@ public class assignOne {
 	public static final int PART_DAY_HOUR = 4;
 	public static final int IS_FULL_TIME =1;
 	public static final int IS_PART_TIME=2;
+	public static final int NO_OF_WORK_DAYS = 20;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to Employee Wage Computation program");
-		double empCheck = Math.floor(Math.random() * 10) % 3;
 		int dailyempwage = 0;
-		int noOfHour =0;
-
+		int monthlywage =0;
+		int noHour =0;
+		System.out.println("Welcome to Employee Wage Computation program");
+		
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 //		if( empCheck == 1){
 //			System.out.println("Employee is present");
 //		}
@@ -26,18 +28,20 @@ public class assignOne {
 	    switch((int)empCheck) {
 	        case IS_FULL_TIME :
 	    	   System.out.println("Employer is present");
-	    	   noOfHour = FULL_DAY_HOUR;
+	    	   noHour = FULL_DAY_HOUR;
 	    	   break;
 	        case IS_PART_TIME:
 	    	   System.out.println("Employer is absent");
-	    	   noOfHour = PART_DAY_HOUR;
+	    	   noHour = PART_DAY_HOUR;
 	    	   break;
 	        default:
 	    	   System.out.println("Employer is absent");
 	    	
 	    }
-	    dailyempwage = WAGE_PER_HOUR * noOfHour;
+	    dailyempwage = noHour * WAGE_PER_HOUR ;
 	    System.out.println("Daily wage-"+ dailyempwage);
+	    monthlywage = dailyempwage * NO_OF_WORK_DAYS;
+	    System.out.println("Monthlywage-" + monthlywage);
 	}
 }
 
