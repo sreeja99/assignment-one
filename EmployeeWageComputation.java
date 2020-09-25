d3063aa29dca4b24a3af9e5f56ef0ae8165538af
sreeja99 <srijagodishala@gmail.com>
2020-09-25 19:55:43 +0530

    Refactored:Created a Class to Calculate the Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -8,26 +8,44 @@ public class assignOne {
 	public static final int IS_PART_TIME=2;
 	public static final int NO_OF_WORK_DAYS = 20;
 	public static final int HOURS_WORKING = 100;
+	
+	public static int computeEmpWage() {
+		int empHrs = 0;
+		int workDays = 0;
+		int totalEmpHrs = 0;
+		while(totalEmpHrs <= HOURS_WORKING && workDays <NO_OF_WORK_DAYS) {
+			workDays++;
+			int empCheck = (int)(Math.floor(Math.random()*10)%2);
+			switch(empCheck) {
+			case IS_FULL_TIME :
+				empHrs = 8;
+				break;
+			case IS_PART_TIME :
+				empHrs = 4;
+				break;
+			default:
+				empHrs =0;
+			}
+			totalEmpHrs += empHrs;
+		}
+			System.out.println("Day:"+ workDays +"\tTotal Working Hours:"+totalEmpHrs);
+			
+//		}
+		int totalEmpWage = totalEmpHrs * WAGE_PER_HOUR;
+		System.out.println("Total Emp Wage:"+totalEmpWage);
+		return totalEmpWage;
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("Welcome to Employee Wage Computation program");
-
-		
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if( empCheck == 1){
-			System.out.println("Employee is present");
-			System.out.println("Wage- "+HOURS_WORKING *WAGE_PER_HOUR);
-		}
-		else {
-			System.out.println("Employee is absent");
-			System.out.println("Wage -"+ NO_OF_WORK_DAYS *PART_DAY_HOUR*WAGE_PER_HOUR);
-		}
-		
-
-
+		computeEmpWage();
 	}
-
 }
+
+
+
+
+
+
 
