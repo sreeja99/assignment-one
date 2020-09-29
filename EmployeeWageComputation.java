bfaf3e2e153d82c72c28b756852b43bcfdddf39b
sreeja99 <52079776+sreeja99@users.noreply.github.com>
2020-09-29 10:01:47 +0530

    Update EmployeeWageComputation.java

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,52 +1,117 @@
 package first;
 
-public class assignOne {
-	public static final int WAGE_PER_HOUR = 20;
-	public static final int FULL_DAY_HOUR = 8;
-	public static final int PART_DAY_HOUR = 4;
-	public static final int IS_FULL_TIME =1;
-	public static final int IS_PART_TIME=2;
-	public static final int NO_OF_WORK_DAYS = 20;
-	public static final int HOURS_WORKING = 100;
-	
-	public static int computeEmpWage(String companyName,int empWagePerHr,int 
-			n0OfWorkingDays,int maxHrsPerMonth) {
-		int empHrs = 0;
-		int workDays = 0;
-		int totalEmpHrs = 0;
-		System.out.println("Days \t Total WorkingHours");
-		while(totalEmpHrs <= HOURS_WORKING && workDays <NO_OF_WORK_DAYS) {
-			workDays++;
-			int empCheck = (int)(Math.floor(Math.random()*10)%2);
-			switch(empCheck) {
-			case IS_FULL_TIME :
-				empHrs = 8;
-				break;
-			case IS_PART_TIME :
-				empHrs = 4;
-				break;
-			default:
-				empHrs =0;
-			}
-			totalEmpHrs += empHrs;
-		    
-		System.out.println( workDays +" \t \t"+totalEmpHrs);
+import java.util.ArrayList;
+import java.util.Map;
+
+public class EmpWageBuilder  implements IComputeEmpWage {
+	// static variables
+	public static final int IS_PART_TIME = 2;
+	public static final int IS_FULL_TIME = 1;
+
+	private int numOfCompanies = 0;
+    private ArrayList<CompanyEmpWage> companyEmpWageList;
+    private Map<String,CompanyEmpWage> companyToEmpWageMap;
+
+	public EmpWageBuilder() {
+		companyEmpWageList = new ArrayList<>();
+		companyToEmpWageMap=new HashMap<>();
+	}
+
+	public void addCompanyEmpWage(int empWagePerHour, int workDaysPerMonth, int workHoursPerMonth, String companyName) {
+		
+		CompanyEmpWage companyEmpWage =new CompanyEmpWage(empWagePerHour,workDaysPerMonth, workHoursPerMonth, companyName); 
+		companyEmpWageList.add(companyEmpWage);
+		companyToEmpWageMap.put(companyName,companyEmpWage);
+	}
+
+	public void ComputeEmpWage() {
+		for (int i = 0; i < numOfCompanies; i++) {
+			CompanyEmpWage companyEmpWage =companyEmpWageList.get(i);
+			companyEmpWage.setTotalWage(this.ComputeEmpWage(companyEmpWage));
+			System.out.println(companyEmpWage);
 			
 		}
-		int totalEmpWage = totalEmpHrs * WAGE_PER_HOUR;
-		System.out.println("Total Emp Wage:"+totalEmpWage);
-		return totalEmpWage;
-		
 	}
+	//@override
+	public int getTotalWage(String companyName) {
+		return companyToEmpWageMap.get(companyName).totalEmpWage;
+	}
+
+	public int ComputeEmpWage(CompanyEmpWage companyEmpWage) {
+		// variables
+		int dayCount = 0;
+		int totalEmpHours = 0;
+		int empHour = 0;
+		// Computation
+		while (empHour <= companyEmpWage.workHoursPerMonth && dayCount < companyEmpWage.workDaysPerMonth) {
+			dayCount++;
+
+			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+
+			switch (empCheck) {
+			case 1:
+				empCheck = IS_FULL_TIME;
+				System.out.println("Employee Present - FULL TIME");
+				empHour = 8;
+				break;
+			case 2:
+				empCheck = IS_PART_TIME;
+				System.out.println("Employee Present - PART TIME");
+				empHour = 4;
+				break;
+			default:
+				System.out.println("Employee is Absent");
+				empHour = 0;
+				break;
+			}
+			totalEmpHours += empHour;
+
+			// Tabular Display of Employee Details for Maximum Monthly Hours or Days
+			System.out.println("Day\t" + dayCount + " Hours Worked\t" + empHour);
+		}
+		return totalEmpHours * companyEmpWage.empWagePerHour;
+	}
+
+public class CompanyEmpWage {
+
+	public final int empWagePerHour;
+	public final int workDaysPerMonth;
+	public final int workHoursPerMonth;
+	public final String companyName;
+	public int totalEmpWage;
+
+	public CompanyEmpWage(int empWagePerHour, int workDaysPerMonth, int workHoursPerMonth, String companyName) {
+		this.companyName = companyName;
+		this.empWagePerHour = empWagePerHour;
+		this.workDaysPerMonth = workDaysPerMonth;
+		this.workHoursPerMonth = workHoursPerMonth;
+	}
+
+	public void setTotalWage(int totalEmpWage) {
+		this.totalEmpWage = totalEmpWage;
+	}
+}
+public interface IComputeEmpWage {
+	public void ComputeEmpWage();
+
+	public void addCompanyEmpWage(int empWagePerHour, int workDaysPerMonth, int workHoursPerMonth, String companyName);
+	public int getTotalWage(String companyName);
+}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		computeEmpWage("Levis",15,25,120);
-		computeEmpWage("Lavie",12,30,95);
+
+		// Welcome Message
+		System.out.println("Welcome to Employee Wage Computation");
+//		IComputeEmpWage empWageBuilder = new EmpWageBuilder();
+		EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+		empWageBuilder.addCompanyEmpWage(20, 15, 100, "Company A");
+		empWageBuilder.addCompanyEmpWage(10, 20, 90, "Company B");
+		empWageBuilder.ComputeEmpWage();
 	}
 }
 
-
+	
 
 
 
